0c038cc455f27d78079b0ca08438e829fa100562
chakradhar06 <chakradhar06@gmail.com>
2026-08-19 00:39:50 -0400

    d

--- vars/javapipeline.groovy
+++ vars/javapipeline.groovy
@@ -48,8 +48,9 @@ def call(Map inputs){
             """
         }
      }
-     stage("Build ${component} image"){
+     stage("Build image"){
        steps{
+         echo "Building ${component}" image"
          withAWS(region:'us-east-1',credentials:'aws-auth') {
           sh"""
            aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${acc_id}.dkr.ecr.us-east-1.amazonaws.com
